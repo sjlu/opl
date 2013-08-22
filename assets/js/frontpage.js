88ec2cea03177f4e9383f58fb82b455b81e5b2b9
Steven Lu <tacticalazn@gmail.com>
2013-08-21 23:40:43 -0400

    All done.

--- assets/js/frontpage.js
+++ assets/js/frontpage.js
@@ -1,62 +1,118 @@
-/**
- * Light Javascript "class" frameworking for you
- * to organize your code a little bit better.
- *
- * If you want more complex things, I'd suggest
- * importing something like Backbone.js as it
- * has much better abilities to handle a MVC
- * like framework including persistant stores (+1)
- *
- * @author  sjlu (Steven Lu)
- */
 var Frontpage = function()
 {
-    /**
-     * The exports variable is responsible for
-     * storing and returning public functions
-     * in the instantized class.
-     */
-    var exports = {};
+  var exports = {};
 
-    /**
-     * Write your public functions like this.
-     * Make sure you include it into the exports
-     * variable.
-     */
-    function public_function() 
-    {
-        /**
-         * Note that we can still call
-         * private functions within the scope
-         * of the "class".
-         */
-        private_function();
+  var interval;
+  var timer = 0;
+
+  var timer_template_html = $('#timer-template').html();
+  var timer_template = Handlebars.compile(timer_template_html);
+
+  function update_timer() {
+    var minutes = Math.floor(timer / 60);
+    var seconds = timer % 60;
+
+    var html = timer_template({
+      minutes: minutes,
+      seconds: seconds
+    });
+
+    $('#timer').html(html);
+  }
+
+  function set_timer(duration) {
+    timer = parseInt(duration)*60;
+
+    $('#timer').show();
+    update_timer();
+
+    interval = setInterval(function() {
+      timer -= 1;
+      update_timer();
+
+      if (!timer) {
+        clearInterval(interval);
+        $('#timer').hide();
+        unbind_keys();
+        show_results();
+      }
+    }, 1000);
+  }
+
+  var results_template_html = $('#results-template').html();
+  var results_template = Handlebars.compile(results_template_html);
+
+  var keys_duration = {};
+  function show_results() {
+    var results = [];
+    for (var key in keys_duration) {
+      var time = keys_duration[key];
+      results.push({
+        key: String.fromCharCode(key),
+        seconds: time / 1000
+      });
     }
-    exports.public_function = public_function;
+    console.log(results);
+    var html = results_template({
+      key_presses: results
+    });
+    $('#results').html(html);
+    $('#results').show();
+  }
 
-    /**
-     * Private functions are very similar, they
-     * just are not included in the exports 
-     * function.
-     */
-     function private_function()
-     {
+  var current_keys_pressed = {};
+  function log_key_time(key, duration) {
+    if (!keys_duration[key]) {
+      keys_duration[key] = 0;
+    }
 
-     }
+    keys_duration[key] += duration;
+  }
 
-     /**
-      * You may wanna have a init() function
-      * to do all your bindings for the class.
-      */
-     function init()
-     {
+  function bind_keys() {
+    $(document).keydown(function (e) {
+      if (current_keys_pressed[e.which]) return;
 
-     }
-     exports.init = init;
+      current_keys_pressed[e.which] = (new Date()).getTime();
+    });
 
-     /**
-      * Last but not least, we have to return
-      * the exports object.
-      */
-     return exports;
+    $(document).keyup(function (e) {
+      var duration =
+        (new Date()).getTime() - current_keys_pressed[e.which];
+
+      log_key_time(e.which, duration);
+
+      delete current_keys_pressed[e.which];
+    });
+  }
+
+  function unbind_keys() {
+    $(document).unbind('keydown');
+    $(document).unbind('keyup');
+
+    var current_time = (new Date()).getTime();
+
+    for (var key in current_keys_pressed) {
+      var start = current_keys_pressed[key];
+      var duration = current_time - start;
+
+      log_key_time(key, duration);
+
+      delete current_keys_pressed[key];
+    }
+  }
+
+  function init()
+  {
+    $('#duration-form').submit(function() {
+      var duration = $('#duration', this).val();
+      $(this).hide();
+      bind_keys();
+      set_timer(duration);
+      return false;
+    });
+  }
+  exports.init = init;
+
+  return exports;
 };
